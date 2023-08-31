82d712f031aa57ecd1442e3a6b810afc56423157
dhasselboeck <d.hasselboeck@im-systems.com>
2023-08-31 12:22:27 +0200

    implement food eating

--- src/graphicsclj/core.clj
+++ src/graphicsclj/core.clj
@@ -2,20 +2,16 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(def ^:const bodyHeight 10)
+(def ^:const bodyWidth 10)
 
-(declare move-x move-y rand-pos-food add-food-when-no-food)
 
-
-#_{:clj-kondo/ignore [:unresolved-symbol]}
-(q/defsketch graphicsclj
-             :title "Snake"
-             :size [500 500]
-             :setup setup
-             :update update-state
-             :draw draw-state
-             :key-pressed handle-user-input
-             :features [:keep-on-top]
-             :middleware [m/fun-mode])
+(declare move-x
+         move-y
+         rand-pos-food
+         add-food-when-no-food
+         check-for-food-eaten
+         detect-collision)
 
 
 (defn setup
@@ -38,6 +34,7 @@
   [state]
   (-> state
       (update-in [:foods] add-food-when-no-food)
+      check-for-food-eaten
       (update-in [:x] (fn [_x] (move-x (:movedir state) _x)))
       (update-in [:y] (fn [_y] (move-y (:movedir state) _y)))))
 
@@ -46,11 +43,14 @@
   [state]
   (q/background 24 24 24)
   (q/fill 255 165 0)
-  (q/rect (:x state) (:y state) 10 10)
+  (q/rect (:x state) (:y state) bodyWidth bodyHeight)
   (q/fill 255 255 255)
   (q/text-size 20)
   (q/text (str (:score state)) 15 30)
-  (doseq [food (:foods state)] ((:renderFn food) 10 10)))
+  (doseq [food (:foods state)] ((:renderFn food) bodyWidth bodyHeight)))
+
+
+(defrecord RandomPosFood [x y renderFn])
 
 
 (defn add-food-when-no-food
@@ -58,13 +58,27 @@
   (if (empty? foods) (conj foods (rand-pos-food)) foods))
 
 
-(defrecord RandomPosFood [x y renderFn])
 (defn rand-pos-food
   []
   (let [rand-pos #(rand-int 439)
         x (rand-pos)
         y (rand-pos)]
     (RandomPosFood. x y (partial q/rect x y))))
+
+
+(defn check-for-food-eaten
+  [state]
+  (if (detect-collision state (first (:foods state)))
+    (assoc state
+      :foods []
+      :score (inc (:score state)))
+    state))
+
+
+(defn detect-collision
+  [{bb1x :x, bb1y :y} {bb2x :x, bb2y :y}]
+  (and (and (>= (+ bodyWidth bb1x) bb2x) (>= (+ bb2x bodyWidth) bb1x))
+       (and (>= (+ bodyHeight bb1y) bb2y) (>= (+ bb2y bodyHeight) bb1y))))
 
 
 (defn move-x
@@ -91,3 +105,15 @@
              498 -498
              3)
        0)))
+
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+(q/defsketch graphicsclj
+             :title "Snake"
+             :size [500 500]
+             :setup setup
+             :update update-state
+             :draw draw-state
+             :key-pressed handle-user-input
+             :features [:keep-on-top]
+             :middleware [m/fun-mode])
