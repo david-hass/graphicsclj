520b6b98410b6891a19f20c2752625441f140e7b
david <david@hasselboeck.de>
2023-09-06 00:55:11 +0200

    render past moves

--- src/graphicsclj/core.clj
+++ src/graphicsclj/core.clj
@@ -2,19 +2,36 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(def ^:const bodyHeight 12)
-(def ^:const bodyWidth 12)
 
-
-(declare move-snake
-         track-head-state
-         create-rand-pos-food
+(declare setup
+         handle-user-input
+         update-state
+         draw-state
          add-food-when-no-food
+         create-rand-pos-food
          eat-food
          increase-score
-         detect-collision
+         move-snake
          move-x
-         move-y)
+         move-y
+         track-head-state
+         detect-collision)
+
+
+(def ^:const body-height 12)
+(def ^:const body-width 12)
+
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+(q/defsketch graphicsclj
+             :title "Snake"
+             :size [500 500]
+             :setup setup
+             :update update-state
+             :draw draw-state
+             :key-pressed handle-user-input
+             :features [:keep-on-top]
+             :middleware [m/fun-mode])
 
 
 (defn setup
@@ -23,8 +40,8 @@
   {:food nil,
    :score 0,
    :movedir "a",
-   :movesTracking [],
-   :bodyParts [{:x 396, :y 396}]})
+   :past-moves [],
+   :snake-head {:x 396, :y 396}})
 
 
 (defn handle-user-input
@@ -39,32 +56,30 @@
 
 (defn update-state
   [state]
-  (-> state
-      (update-in [:food] add-food-when-no-food)
-      ;((fn [x] (println x) x))
-      (#(update-in % [:food] eat-food (first (:bodyParts %))))
-      ;((fn [x] (println x) x))
-      (#(update-in % [:score] increase-score (:food %)))
-      ;((fn [x] (println x) x))
-      (#(update-in %
-                   [:movesTracking]
-                   track-head-state
-                   (:score %)
-                   (first (:bodyParts %))))
-      (#(update-in % [:bodyParts 0] move-snake (:movedir %)))
-      ((fn [x] (println x) x))))
+  (->
+    state
+    (update-in [:food] add-food-when-no-food)
+    (#(update-in % [:food] eat-food (:snake-head %)))
+    (#(update-in % [:score] increase-score (:food %)))
+    (#(update-in % [:snake-head] move-snake (:movedir %)))
+    (#(update-in % [:past-moves] track-head-state (:score %) (:snake-head %)))))
 
 
 (defn draw-state
   [state]
   (q/background 24 24 24)
-  (q/fill 255 165 0)
-  (doseq [bodyPart (:bodyParts state)]
-    (q/rect (:x bodyPart) (:y bodyPart) bodyWidth bodyHeight))
   (q/fill 255 255 255)
   (q/text-size 20)
   (q/text (str (:score state)) 15 30)
-  ((get (:food state) :renderFn +) bodyWidth bodyHeight))
+  ((get (:food state) :renderFn +) body-width body-height)
+  (q/fill 255 165 0)
+  (q/stroke-weight 0)
+  (q/rect (:x (:snake-head state))
+          (:y (:snake-head state))
+          body-width
+          body-height)
+  (doseq [past-move (:past-moves state)]
+    (q/rect (:x past-move) (:y past-move) body-width body-height)))
 
 
 (defn add-food-when-no-food
@@ -83,27 +98,7 @@
 (defn eat-food [food head] (if (detect-collision food head) nil food))
 
 
-(defn detect-collision
-  [{bb1x :x, bb1y :y} {bb2x :x, bb2y :y}]
-  (and (and (>= (+ bodyWidth bb1x) bb2x) (>= (+ bb2x bodyWidth) bb1x))
-       (and (>= (+ bodyHeight bb1y) bb2y) (>= (+ bb2y bodyHeight) bb1y))))
-
-
 (defn increase-score [score food] (if (some? food) score (inc score)))
-
-
-(defn track-head-state
-  [movesTracking score head]
-  ()
-  (take score
-        (concat (let [[cur] movesTracking]
-                  (if (or (nil? cur)
-                              (not (= (:dir cur) (:dir head)))
-                              (or (= (Math/abs (- (:x head) (:x cur))) 12)
-                                  (= (Math/abs (- (:y head) (:y cur))) 12)))
-                    [head]
-                    []))
-                movesTracking)))
 
 
 (defn move-snake
@@ -112,9 +107,6 @@
     :dir movedir
     :x (move-x movedir (:x head))
     :y (move-y movedir (:y head))))
-
-
-
 
 
 (defn move-x
@@ -143,13 +135,20 @@
        0)))
 
 
-#_{:clj-kondo/ignore [:unresolved-symbol]}
-(q/defsketch graphicsclj
-             :title "Snake"
-             :size [500 500]
-             :setup setup
-             :update update-state
-             :draw draw-state
-             :key-pressed handle-user-input
-             :features [:keep-on-top]
-             :middleware [m/fun-mode])
+(defn track-head-state
+  [past-moves score head]
+  (take score
+        (concat
+          (let [[cur] past-moves]
+            (cond (nil? cur) [head]
+                  (> (Math/abs (- (:x head) (:x cur))) (- body-width 1)) [head]
+                  (> (Math/abs (- (:y head) (:y cur))) (- body-height 1)) [head]
+                  :else []))
+          past-moves)))
+
+
+(defn detect-collision
+  [{bb1x :x, bb1y :y} {bb2x :x, bb2y :y}]
+  (and (and (>= (+ body-width bb1x) bb2x) (>= (+ bb2x body-width) bb1x))
+       (and (>= (+ body-height bb1y) bb2y) (>= (+ bb2y body-height) bb1y))))
+
