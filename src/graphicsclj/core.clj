f5c60ffdccecda20ec38d8816df27892ae389c7c
dhasselboeck <d.hasselboeck@im-systems.com>
2023-09-06 15:33:50 +0200

    bite-tail

--- src/graphicsclj/core.clj
+++ src/graphicsclj/core.clj
@@ -3,6 +3,16 @@
             [quil.middleware :as m]))
 
 
+(def ^:const window-height 500)
+(def ^:const window-width 500)
+(def ^:const body-len 24)
+(def ^:const step (/ body-len 4))
+(def ^:const max-y
+  (first (filter #(= 0 (mod % step)) (iterate dec window-height))))
+(def ^:const max-x
+  (first (filter #(= 0 (mod % step)) (iterate dec window-width))))
+
+
 (declare setup
          handle-user-input
          update-state
@@ -15,18 +25,8 @@
          move-x
          move-y
          track-head-state
+         bite-tail
          detect-collision)
-
-(def ^:const window-height 500)
-(def ^:const window-width 500)
-(def ^:const body-height 12)
-(def ^:const body-width 12)
-(def ^:const step 3)
-(def ^:const max-y
-  (first (filter #(= 0 (mod % step)) (iterate dec window-height))))
-(def ^:const max-x
-  (first (filter #(= 0 (mod % step)) (iterate dec window-width))))
-
 
 
 #_{:clj-kondo/ignore [:unresolved-symbol]}
@@ -44,11 +44,11 @@
 (defn setup
   []
   (q/frame-rate 60)
-  {:food nil,
+  {:food (create-rand-pos-food),
    :score 0,
    :movedir "a",
    :past-moves [],
-   :snake-head {:x (- max-x (* step 15)), :y (- max-y (* step 15))}})
+   :head {:x (- max-x (* step 15)), :y (- max-y (* step 15))}})
 
 
 (defn handle-user-input
@@ -57,18 +57,23 @@
     (:w :up) (if (= (:movedir state) "s") state (assoc state :movedir "w"))
     (:s :down) (if (= (:movedir state) "w") state (assoc state :movedir "s"))
     (:a :left) (if (= (:movedir state) "d") state (assoc state :movedir "a"))
-    (:d :right) (if (= (:movedir state) "a")  state (assoc state :movedir "d"))
+    (:d :right) (if (= (:movedir state) "a") state (assoc state :movedir "d"))
     state))
 
 
 (defn update-state
   [state]
-  (-> state
-      (update-in [:food] add-food-when-no-food)
-      (#(update-in % [:food] eat-food (:snake-head %)))
-      (#(update-in % [:score] increase-score (:food %)))
-      (#(update-in % [:past-moves] track-head-state (:score %) (:snake-head %)))
-      (#(update-in % [:snake-head] move-snake (:movedir %)))))
+  (let [new-state
+          (->
+            state
+            (#(update-in % [:food] eat-food (:head %)))
+            (#(update-in % [:score] increase-score (:food %)))
+            (#(update-in % [:past-moves] track-head-state (:score %) (:head %)))
+            (#(update-in % [:head] move-snake (:movedir %)))
+            (update-in [:food] add-food-when-no-food))]
+    (if (bite-tail (:head new-state) (:past-moves new-state))
+      (setup)
+      new-state)))
 
 
 (defn draw-state
@@ -77,14 +82,11 @@
   (q/fill 255 255 255)
   (q/text-size 20)
   (q/text (str (:score state)) 15 30)
-  ((get (:food state) :renderFn +) body-width body-height)
+  (q/rect (:x (:food state)) (:y (:food state)) body-len body-len)
   (q/fill 255 165 0)
-  (q/rect (:x (:snake-head state))
-          (:y (:snake-head state))
-          body-width
-          body-height)
+  (q/rect (:x (:head state)) (:y (:head state)) body-len body-len)
   (doseq [past-move (:past-moves state)]
-    (q/rect (:x past-move) (:y past-move) body-width body-height)))
+    (q/rect (:x past-move) (:y past-move) body-len body-len)))
 
 
 (defn add-food-when-no-food
@@ -94,9 +96,9 @@
 
 (defn create-rand-pos-food
   []
-  (let [x (+ (* step 10) (rand-int (- window-width (* step 10))))
-        y (+ (* step 10) (rand-int (- window-height (* step 10))))]
-    {:x x, :y y, :renderFn (partial q/rect x y)}))
+  (let [x (+ (* step 10) (rand-int (- window-width (* step 20))))
+        y (+ (* step 10) (rand-int (- window-height (* step 20))))]
+    {:x x, :y y}))
 
 
 (defn eat-food [food head] (if (detect-collision food head) nil food))
@@ -136,15 +138,21 @@
   (take score
         (concat (let [[cur] past-moves]
                   (cond (nil? cur) [head]
-                        (>= (Math/abs (- (:x head) (:x cur))) body-width) [head]
-                        (>= (Math/abs (- (:y head) (:y cur))) body-height)
-                          [head]
+                        (>= (Math/abs (- (:x head) (:x cur))) body-len) [head]
+                        (>= (Math/abs (- (:y head) (:y cur))) body-len) [head]
                         :else []))
                 past-moves)))
 
 
+(defn bite-tail
+  [head past-moves]
+  (println head past-moves)
+  
+  (if (some (partial detect-collision head) past-moves) true false))
+
+
 (defn detect-collision
   [{bb1x :x, bb1y :y} {bb2x :x, bb2y :y}]
-  (and (and (>= (+ body-width bb1x) bb2x) (>= (+ bb2x body-width) bb1x))
-       (and (>= (+ body-height bb1y) bb2y) (>= (+ bb2y body-height) bb1y))))
+  (and (and (>= (+ body-len bb1x) bb2x) (>= (+ bb2x body-len) bb1x))
+       (and (>= (+ body-len bb1y) bb2y) (>= (+ bb2y body-len) bb1y))))
 
